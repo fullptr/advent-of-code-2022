f9174fd3a3ad0e62cf980e201bb852179735fac0
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2022-12-05 20:04:55 +0000

    Clean day 5 implementation

--- day05.py
+++ day05.py
@@ -8,30 +8,27 @@ def move1(columns, count, src, dst):
         columns[dst].append(columns[src].pop())
 
 def move2(columns, count, src, dst):
-    temp = [columns[src].pop() for _ in range(count)]
+    columns[src], temp = columns[src][0:-count], columns[src][-count]
+    #temp = [columns[src].pop() for _ in range(count)]
     columns[dst].extend(reversed(temp))
 
 columns1 = []
 commands = []
-
 for line in lines:
     if line.strip().startswith("["):
-        # Replace empty columns with underscores, strip out all spaces and brackets
-        line = line.replace("    ", " [_]").replace("[", "").replace("]", "").replace(" ", "")
+        line = line[1::4]
         if len(columns1) == 0: # init column list
             columns1 = [[] for _ in line]
         for idx, char in enumerate(line):
-            columns1[idx].append(char)
+            if char != " ":
+                columns1[idx].append(char)
         pass
     elif line.startswith("move"):
         match = parse.parse("move {move:d} from {from:d} to {to:d}", line)
         commands.append((match["move"], match["from"]-1, match["to"]-1))
 
-# flip and pop off the underscore placeholders
 for column in columns1:
     column.reverse()
-    while column and column[-1] == "_":
-        column.pop()
 
 columns2 = copy.deepcopy(columns1)
 
