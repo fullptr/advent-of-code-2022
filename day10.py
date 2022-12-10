2a7abb926e9ce26963f223c41a0d5ec42c1496f6
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2022-12-10 14:42:27 +0000

    day10 cleanup

--- day10.py
+++ day10.py
@@ -7,32 +7,25 @@ jobs = Queue()
 
 for line in data:
     match line.split():
-        case ["noop"]: jobs.put(["noop", 1])
-        case ["addx", value]: jobs.put(["addx", 2, int(value)])
+        case ["noop"]: jobs.put([1, 0]) # steps, addx value
+        case ["addx", value]: jobs.put([2, int(value)])
 
 reg = 1
-current_job = None
 cycle = 1
 signal_sum = 0
-
 crt_screen = [" " for _ in range(40*6)]
 
-while not jobs.empty() or current_job is not None:
-    if current_job is None and not jobs.empty():
-        current_job = jobs.get()
-
-    if cycle % 40 - 20 == 0:
-        signal_sum += cycle*reg
-    if abs((cycle - 1) % 40 - reg) <= 1:
+current_job = jobs.get()
+while current_job:
+    if cycle % 40 - 20 == 0: # part 1
+        signal_sum += cycle * reg
+    if abs((cycle - 1) % 40 - reg) <= 1: # part 2
         crt_screen[cycle - 1] = '█'
 
-    current_job[1] -= 1 # tick down
-    if current_job[1] == 0:
-        match current_job[0]:
-            case "noop": pass
-            case "addx": reg += current_job[2]
-        current_job = None
-
+    current_job[0] -= 1 # tick down
+    if current_job[0] == 0:
+        reg += current_job[1]
+        current_job = jobs.get() if not jobs.empty() else None
     cycle += 1
 
 print(signal_sum)
