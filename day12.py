3a2b882b52acde0d9d96482d61a9fdb1f44ca546
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2022-12-19 00:14:06 +0000

    Solve day12

--- day12.py
+++ day12.py
@@ -18,13 +18,6 @@ def find(c):
 S = find("S")
 E = find("E")
 
-offsets = [
-    (0, 1),
-    (0, -1),
-    (1, 0),
-    (-1, 0),
-]
-
 def level_of(pos):
     x, y = pos
     idx = rows[y][x]
@@ -34,31 +27,35 @@ def valid(pos):
     x, y = pos
     return 0 <= x < width and 0 <= y < height
 
-def can_move_to_neighbour(curr, neighbour):
-    return level_of(neighbour) <= level_of(curr) + 1
+# Neighbour can either climb up 1 to us, or drop down to us
+def can_move_from_neighbour(curr, neighbour):
+    return level_of(curr) - 1 <= level_of(neighbour)
 
-def neighbours(pos):
-    x, y = pos
-    for dx, dy in offsets:
+def neighbours(x, y):
+    for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
         neighbour = (x + dx), (y + dy)
-        if valid(neighbour) and can_move_to_neighbour(pos, neighbour):
+        if valid(neighbour) and can_move_from_neighbour((x, y), neighbour):
             yield neighbour
 
 dist = {}
 queue = SimpleQueue()
-dist[S] = 0
+dist[E] = 0
 
-queue.put(S)
+queue.put(E)
 while not queue.empty():
-    pos = queue.get()
-    x, y = pos
-
-    for neighbour in neighbours(pos):
+    x, y = queue.get()
+    for neighbour in neighbours(x, y):
         if neighbour not in dist:
-            dist[neighbour] = dist[pos] + 1
+            dist[neighbour] = dist[x, y] + 1
             queue.put(neighbour)
 
-print(dist[E])
+print(dist[S])
+min_dist = dist[S]
+for (x, y), distance in dist.items():
+    if rows[y][x] == "a" and distance < min_dist:
+        min_dist = distance
+
+print(min_dist)
 
 
 
